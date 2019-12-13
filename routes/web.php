d8c8b8a2efc32369383c286d70cf2f8bd302734a
satria girinanda <satriagn93@gmail.com>
2019-12-13 10:25:43 +0700

    Auth Login

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,19 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/siswa','SiswaController@index');
-Route::post('/siswa/create','SiswaController@create');
-Route::get('/siswa/{id}/edit','SiswaController@edit');
-Route::post('/siswa/{id}/update','SiswaController@update');
-Route::get('/siswa/{id}/delete','SiswaController@delete');
+Route::get('/login','AuthController@login')->name('login');
+Route::post('/postlogin','AuthController@postlogin');
+Route::get('/logout','AuthController@logout');
+
+Route::group(['middleware' => 'auth'],function (){
+    Route::get('/dashboard','DashboardController@index');
+    Route::get('/siswa','SiswaController@index');
+    Route::post('/siswa/create','SiswaController@create');
+    Route::get('/siswa/{id}/edit','SiswaController@edit');
+    Route::post('/siswa/{id}/update','SiswaController@update');
+    Route::get('/siswa/{id}/delete','SiswaController@delete');
+
+});
